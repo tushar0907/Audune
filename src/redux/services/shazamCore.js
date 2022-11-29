fbba3af3f80201924faffe1f449cb15cbc9cc324
Elite Programmer <tusharkumar0807@gmail.com>
2022-11-30 02:48:04 +0530

    details page

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
-
 export const shazamCoreApi=createApi({
       reducerPath: 'shazamCoreApi',
       baseQuery: fetchBaseQuery({
@@ -14,9 +13,22 @@ export const shazamCoreApi=createApi({
       }),
       endpoints: (builder) => ({
             getTopCharts: builder.query({query: () => '/charts/world'}),
+            getSongsByGenre: builder.query({query: (genre) => `/charts/genre-world?genre_code=${genre}`}),
+            getSongsByCountry: builder.query({query: (countryCode) => `/charts/country?country_code=${countryCode}`}),
+            getSongsBySearch: builder.query({query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`}),
+            getArtistDetails: builder.query({query: (artistId) => `/artists/details?artist_id=${artistId}`}),
+            getSongDetails: builder.query({query: ({songid}) => `/tracks/details?track_id=${songid}`}),
+            getSongRelated: builder.query({query: ({songid}) => `/tracks/related?track_id=${songid}`}),
       }),
 });
 
+
 export const {
       useGetTopChartsQuery,
+      useGetSongsByGenreQuery,
+      useGetSongsByCountryQuery,
+      useGetSongsBySearchQuery,
+      useGetArtistDetailsQuery,
+      useGetSongDetailsQuery,
+      useGetSongRelatedQuery,
 }=shazamCoreApi;
