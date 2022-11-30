9ef2edc925b3e46e62a29094b746f27cdea0c08a
Elite Programmer <tusharkumar0807@gmail.com>
2022-11-30 13:51:19 +0530

    final changes

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -13,7 +13,7 @@ import 'swiper/css';
 import 'swiper/css/free-mode';
 
 const TopChartCard=({song, i, isPlaying, activeSong, handlePauseClick, handlePlayClick}) => (
-  <div className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${activeSong?.title===song?.title? 'bg-[#4c426e]':'bg-transparent'} py-2 p-4 rounded-lg cursor-pointer mb-2`}>
+  <div className={`w-full flex flex-row items-center hover:bg-[#1f1f20] ${activeSong?.title===song?.title? 'bg-[#4c426e]':'bg-transparent'} py-2 p-4 rounded-lg cursor-pointer mb-2`}>
     <h3 className="font-bold text-base text-white mr-3">{i+1}.</h3>
     <div className="flex-1 flex flex-row justify-between items-center">
       <img className="w-20 h-20 rounded-lg" src={song?.images?.coverart} alt={song?.title} />
